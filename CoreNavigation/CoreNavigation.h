85c017c4b2bd454bb7bec12511eeff32110063d4
Auzan Lazuardi <auzan.lazuardi@mola.tv>
2024-02-24 15:38:02 +0700

    commit

---
+++ CoreNavigation/CoreNavigation.h
@@ -0,0 +1,18 @@
+//
+//  CoreNavigation.h
+//  CoreNavigation
+//
+//  Created by Auzan Lazuardi on 24/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreNavigation.
+FOUNDATION_EXPORT double CoreNavigationVersionNumber;
+
+//! Project version string for CoreNavigation.
+FOUNDATION_EXPORT const unsigned char CoreNavigationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreNavigation/PublicHeader.h>
+
+
